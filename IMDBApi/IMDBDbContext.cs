60a8d93351333d1f7d41063560113b744550e4d5
AlexanderHoeystad <Alek369@hotmail.com>
2024-10-16 12:08:33 +0200

    dbcontext done

--- IMDBApi/IMDBDbContext.cs
+++ IMDBApi/IMDBDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IMDBApi;
+using Microsoft.EntityFrameworkCore;
+using System;
 
-namespace IMDBApi
+public class IMDBDbContext : DbContext
 {
-    public class IMDBDbContext : DbContext
-    {
-
-    }
+    public IMDBDbContext(DbContextOptions<IMDBDbContext> options) : base(options) { }
+    public DbSet<Title> Titles { get; set; }
+    public DbSet<Crew> Crews { get; set; }
+    public DbSet<Name> Names { get; set; }
 }
+
